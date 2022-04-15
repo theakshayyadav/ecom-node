d9ac77e513d40e0f2b20480472433da79480e9e3
Akshay Sanjay Yadav <94215067+theakshayyadav@users.noreply.github.com>
2022-04-15 15:58:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express= require("express");
 const mongoose = require("mongoose");
 
 var app= express();
-mongoose.connect("mongodb+srv://akshay:Akki0000@cluster0.1bevl.mongodb.net/ecommerce")
+mongoose.connect("mongodb+srv://akshay:Akki0000@cluster0.1bevl.mongodb.net/ecom")
 var jasonparser = body_parser.json();
 app.use(body_parser.json({limit:'50mb'}));
 app.use(body_parser.urlencoded({limit:'50mb', extended: true}));
